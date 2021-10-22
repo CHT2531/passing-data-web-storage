9d2263b42e69775420fcd7e86e140a58d33e2908
mmantle-hud <m.e.mantle@hud.ac.uk>
2021-10-22 16:37:40 +0100

    Add files via upload

---
+++ js/details.js
@@ -0,0 +1,27 @@
+function loadData(url,callback)
+{
+	fetch(url).then(function(response) {
+		return response.json();
+	}).then(function(json) {
+		callback(json)
+	});
+} //end of ajax()
+
+function populateContent(country)
+{
+	const titleEl=document.getElementById("title");
+	const capitalEl=document.getElementById("capital");
+	const populationEl=document.getElementById("population");
+	titleEl.textContent = country.name;
+	capitalEl.textContent = country.capital;
+	populationEl.textContent = country.population;
+}
+
+function init(){
+	//get the chosen country's id from session storage
+	const id = sessionStorage.getItem("id");
+	loadData("data/country"+id+".json",populateContent); //request a JSON file e.g. country3.json
+}
+
+
+init();
